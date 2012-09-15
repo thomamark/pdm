aa1f13f2f5c3a99a8b798a82a42c0a7a8646517e
Mark Thomas <mr.thomas@gmail.com>
2012-09-15 14:40:17 -0700

    adding session support.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Pdm::Application.routes.draw do
+  resources :sessions, only: [:new, :create, :destroy]
   get "static_pages/home"
-   
   root to: 'static_pages#home'
+
+
+  match '/restart', to: 'sessions#destroy', via: :delete
 end
