b515a1e9d6a9ed18249ada8451576e4943408b62
Mark Thomas <mr.thomas@gmail.com>
2013-09-02 20:53:32 -0700

    scores and map image fixing.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,3 +1,4 @@
+require 'active_support/core_ext/integer/inflections'
 class StaticPagesController < ApplicationController
   def home
 	@tt = Score.order("score DESC").limit(9)
@@ -7,17 +8,40 @@ class StaticPagesController < ApplicationController
 	[@tt, @tt_today].each do |a|
 		if a.length() < 9
 			(9-a.length()).times do
-				a << Score.new(name: "AAA", score: 0.0)
+				a << Score.new(name: "???", score: 0.0)
 			end
 		end
 	end
-
   end
 
   def play
   end
 
   def lb
+  end
+
+  def gmm
+  end
+
+  def rank
+	@tt = Score.order("score DESC").limit(9)
+	@tt_today = Score.where(created_at: Time.now.midnight..(Time.now.midnight + 1.day)).order("score DESC").limit(9)
+
+	begin
+		@coverage = params[:c].to_f
+	rescue
+		@coverage = 0.0
+	end
+	
+	@today = @tt_today.index(@tt_today.find { |s| s.score <= @coverage})
+	@today = if @today.nil? then "Embarrassment"  else (@today+1).ordinalize end
+
+	@overall = @tt.index(@tt.find { |s| s.score <= @coverage})
+	@overall = if @overall.nil? then "Embarrassment"  else (@overall+1).ordinalize end
+
+	@result = {:today => @today, :overall => @overall, :coverage => @coverage}
+
+	render json: @result
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Pdm::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/play"
+  get "static_pages/rank"
 
   resources :scores
 
   if Rails.env.development?
     get "static_pages/lb"
+    get "static_pages/gmm"
     match "/lb" => "static_pages#lb" 
+    match "/gmm" => "static_pages#gmm" 
   end
 
   match "/play" => "static_pages#play"
+  match "/myRank" => "static_pages#rank"
   root to: 'static_pages#home'
 
 end
